780f9a8160914e94e28d21e7df4a5af40e1a03c2
Peipi98 <39802340+Peipi98@users.noreply.github.com>
2022-11-05 12:52:45 +0100

    changes

---
+++ libs/utils.py
@@ -0,0 +1,8 @@
+import random
+
+def problem(N, seed=None):
+    random.seed(seed)
+    return [
+        list(set(random.randint(0, N - 1) for n in range(random.randint(N // 5, N // 2))))
+        for n in range(random.randint(N, N * 5))
+    ]
